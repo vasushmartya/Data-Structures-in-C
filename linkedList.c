f9141343c8e184b95cbea7b3a30c6dfea891efad
vasushmartya <vasushmartya@gmail.com>
2025-11-09 01:37:18 +0530

    Implement insertToEnd and tailNode functions
    Added functions to insert a node at the end and find the tail of the linked list.

--- linkedList.c
+++ linkedList.c
@@ -7,40 +7,61 @@ typedef struct Node{
 }Node;
 
 void LinkedListTraversal(Node* ptr){
+    printf("\n");
     while(ptr != NULL){
         printf("%d ", ptr->data);
         ptr = ptr->next;
     }
 }
 
-int main(){
-    Node* head;
-    Node* second;
-    Node* tail;
+Node* tailNode(Node* ptr){
+    if (ptr == NULL)
+        return NULL;
 
-    head = (Node*)malloc(sizeof(Node));
-    second = (Node*)malloc(sizeof(Node));
-    tail = (Node*)malloc(sizeof(Node));
-
-    if (head == NULL || second == NULL || tail == NULL) {
-        printf("Memory allocation failed\n");
-        exit(1);
+    while (ptr->next != NULL) {
+        ptr = ptr->next;
     }
+    return ptr;
+}
 
-    head->data = 11;
-    head->next = second;
+void insertToEnd(Node** head){
+    Node* ptr;
+    ptr = (Node*)malloc(sizeof(Node));
+    if (ptr == NULL) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    int data;
 
-    second->data = 13;
-    second->next = tail;
+    printf("\nEnter number to store: ");
+    scanf("%d", &data);
 
-    tail->data = 17;
-    tail->next = NULL;
+    ptr->data = data;
+    ptr->next = NULL;
+
+    if (*head == NULL) {
+        *head = ptr;
+    } else {
+        Node* tail = tailNode(*head);
+        tail->next = ptr;
+    }
+    printf("\nInserted Successfully");
+}
+
+int main(){
+    Node* head = NULL;
+
+    insertToEnd(&head);
+    insertToEnd(&head);
+    insertToEnd(&head);
 
     LinkedListTraversal(head);
 
-    free(head);
-    free(second);
-    free(tail);
-    
+    Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
     return 0;
 }
